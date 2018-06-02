e70827ae006c547ab5e4701cad4d85f75fb9c2dc
Tinywan <756684177@qq.com>
2018-06-02 18:30:49 +0800

    tp5 任务队列 think-queue

---
+++ application/common/queue/Job1.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/6/2
+ * Time: 16:24
+ */
+
+class Job1
+{
+
+}
---
+++ application/common/queue/Worker.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/6/2
+ * Time: 16:30
+ */
+
+namespace app\common\queue;
+
+
+class Worker
+{
+
+}
