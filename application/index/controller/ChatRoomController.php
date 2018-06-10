eec46be198e2e91044e044758063f56b6475453d
Tinywan <756684177@qq.com>
2018-06-10 21:34:10 +0800

    利用GatewayClient发送 系统通告信息

--- application/index/controller/ChatRoomController.php
+++ application/index/controller/ChatRoomController.php
@@ -19,9 +19,11 @@ use think\Log;
 
 class ChatRoomController extends Controller
 {
-    public $userId = 12;
+    public $userId = 2589272172487;
 
-    public $groupId = 1;
+    public $userName = 'Tinywan';
+
+    public $roomId = 'L06777';
 
     //
     public function index()
@@ -45,23 +47,26 @@ class ChatRoomController extends Controller
         Gateway::joinGroup($client_id, $group_id);
     }
 
-    // mvc后端发消息 利用GatewayClient发送 Events.php
-    // {"type":"say","roomId":"1002","userId":"88","content":"Welcome Live Room"}
+    /**
+     * 利用GatewayClient发送 系统通告信息
+     * curl http://notes.env/index/chat_room/sendMessage
+     */
     public function sendMessage()
     {
         // stream_socket_client(): unable to connect to tcp://127.0.0.1:1236
         $uid = $this->userId;
-        $group = $this->groupId;
-        $message = json_encode([
-          'type'=>'say',
-          'msg'=>'mvc后端发消息 Hello ThinkPHP5'
-        ]);
+        $roomId = $this->roomId;
+        $serverTime = date('Y-m-d H:i:s', time());
+        $resData = [
+          'type' => 'notice',
+          'roomId' => $roomId,
+          'userName' => $this->userName,
+          'msg' => "这是系统公告信息！！！", // 发送给客户端的消息，而不是聊天发送的内容
+          'joinTime' => $serverTime // 加入时间
+        ];
         // 设置GatewayWorker服务的Register服务ip和端口，请根据实际情况改成实际值
         Gateway::$registerAddress = '127.0.0.1:1238';
-        // 向任意uid的网站页面发送数据
-        Gateway::sendToUid($uid, $message);
-        json_encode($uid,JSON_FORCE_OBJECT);
         // 向任意群组的网站页面发送数据，如果开启，则会向页面发送两条一样的消息
-        //Gateway::sendToGroup($group, $message);
+        Gateway::sendToGroup($roomId, json_encode($resData));
     }
 }
